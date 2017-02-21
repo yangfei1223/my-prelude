3169f34b3f1073b46274c37003b36e94dfef322c
yangfei1223 <yangfei92516@163.com>
2017-02-21 14:04:30 +0800

    add custom.el and prelude-modules.el in github.

---
+++ personal/custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-minimum-prefix-length 1)
+ '(org-agenda-custom-commands nil)
+ '(org-agenda-files
+   (quote
+    ("~/org-notes/gtd.org" "~/org-notes/emacs.org" "~/org-notes/notes.org" "~/org-notes/learning.org" "~/org-notes/vim.org" "~/org-notes/journal.org")))
+ '(org-agenda-ndays 1)
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-deadline-prewarning-if-scheduled t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-start-on-weekday nil)
+ '(org-deadline-warning-days 14)
+ '(org-fast-tag-selection-single-key (quote expert))
+ '(org-reverse-note-order t)
+ '(package-selected-packages
+   (quote
+    (switch-window evil-jumper evil-matchit use-package function-args google-c-style srefactor company-c-headers cmake-font-lock company-irony stickyfunc-enhance rtags flycheck-irony irony cmake-mode w3m org-download worf ox-reveal org-octopress window-numbering counsel solarized-theme monokai-theme 2048-game d-mode evil-nerd-commenter evil-leader auto-yasnippet popwin org-pomodor org-pomodoro iedit swiper hungry-delete markdown-mode yaml-mode web-mode scss-mode geiser ensime scala-mode yari inf-ruby ruby-tools company-anaconda anaconda-mode flycheck-ocaml merlin utop tuareg company-auctex cdlatex auctex json-mode js2-mode haskell-mode gotest go-projectile go-eldoc company-go go-mode alchemist elixir-mode erlang rainbow-mode elisp-slime-nav slime coffee-mode cider clojure-mode rainbow-delimiters evil-numbers evil-visualstar evil-surround evil mediawiki key-chord company helm-ag helm-descbinds helm-projectile helm smex ido-ubiquitous flx-ido zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(which-key-idle-delay 0.4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
